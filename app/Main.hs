270fcc24ecfee3c27ef13d3005e702ad333d7ed1
Paul Gardiner <paul@glidos.site>
2018-12-28 16:42:03 +0000

    Add functions related to constructing backups from diffs
    Given a seed day and a set of diffs, calculate all the sequences of diffs
    for which the first has a from day matching the seed and the subsequent
    ones have from day matching the to day of the previous diff. From such a
    set of sequences, we can work out which backup days can be created and
    which diffs are necessary to do so.

--- app/Main.hs
+++ app/Main.hs
@@ -43,3 +43,22 @@ removeOldCopies :: IO ()
 removeOldCopies =
     let removeOldCopiesFor freq =  traverse_ remove =<< dropFromEnd (numCopies freq) . sort <$> backupsForPeriod freq
     in traverse_ removeOldCopiesFor [Daily, Weekly, Monthly]
+
+-- Given a seed day and a sequence of diffs, return the day of the backup that can be generated.
+-- There is no need to check that each diff follows from the preceding one and the first from the
+-- seed because this function is used only in cases where that is the case
+inferredDay :: Day -> [Diff] -> Day
+inferredDay seed [] = seed
+inferredDay _ (Diff _ to : _) = to
+
+-- Given a seed day and a set of diffs, return the set of possible inference sequences
+-- This allows us to work out what backups can be created from a set of diffs and for
+-- each such backup, which of those diffs are needed.
+inferences :: Day -> [Diff] -> [[Diff]]
+inferences seed diffs = let from (Diff f _) = f
+                            extend infs = [d:ds | ds <- infs, d <- diffs, inferredDay seed ds == from d]
+                        in concat $ takeWhile (not . null) $ iterate extend [[]]
+
+-- For when we just need to know which are constructable but not how
+constructable :: Day -> [Diff] -> [Day]
+constructable seed diffs = inferredDay seed <$> inferences seed diffs
